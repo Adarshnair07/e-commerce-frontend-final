65591e9b5bafb5f4b0f7b29947ca089babb86285
nikhileashy <nikhileashy9@gmail.com>
2024-03-13 22:29:02 +0530

    Added categories

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useEffect, useLayoutEffect, useRef, useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import logo from '../Assets/logo.png'
@@ -7,12 +7,18 @@ import { ShopContext } from '../../Context/ShopContext'
 import nav_dropdown from '../Assets/nav_dropdown.png'
 
 const Navbar = () => {
-
+  const [categories,setCategories] = useState([]);
   let [menu,setMenu] = useState("shop");
   const {getTotalCartItems} = useContext(ShopContext);
 
   const menuRef = useRef();
 
+  useLayoutEffect(()=>{
+    fetch('http://localhost:4000/categories')
+    .then((res) => res.json())
+    .then((data) => setCategories(data))
+    .then(console.log(categories))
+  },[])
   const dropdown_toggle = (e) => {
     menuRef.current.classList.toggle('nav-menu-visible');
     e.target.classList.toggle('open');
@@ -30,6 +36,7 @@ const Navbar = () => {
         <li onClick={()=>{setMenu("mens")}}><Link to='/mens' style={{ textDecoration: 'none' }}>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link to='/womens' style={{ textDecoration: 'none' }}>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{ textDecoration: 'none' }}>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        {categories.map((item,i) => <li onClick={()=>{setMenu(item.id)}} key={i}><Link to={`/shopcategory/${item.id}`} style={{ textDecoration: 'none' }}>{item.name}</Link>{menu===item.id?<hr/>:<></>}</li>)}
       </ul>
       <div className="nav-login-cart">
         {localStorage.getItem('auth-token')
